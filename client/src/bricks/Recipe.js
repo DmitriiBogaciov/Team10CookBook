2cc1c960dc88acbe00cf9c493faefa9c4f442c6b
Dmitrii Bogaciov <dmitrii.bogaciov@gmail.com>
2023-04-13 18:13:54 +0200

    add button show details

--- client/src/bricks/Recipe.js
+++ client/src/bricks/Recipe.js
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from 'react-bootstrap/Card';
 import Icon from '@mdi/react';
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import { mdiGlassTulip, mdiReceiptTextEditOutline } from '@mdi/js';
 import IngredientList from "./IngredientList";
+import RecipeDetail from "./RecipeDetail";
 
 function Recipe(props) {
-    return (
-       <Card>
-           <Card.Body>
-               <div>
-                   <Icon path={mdiGlassTulip} size={1} color="red" />{" "}
-                   {props.recipe.name}
-               </div>
-               <div>
-                   <Icon path={mdiReceiptTextEditOutline} size={1} />
-                   {props.recipe.description}
-               </div>
+    const [show, setShow] = useState(false);
 
-               <div>
-                   Category: {props.recipe.categoryIdList}
-               </div>
-               <div>
-                   Ingredients: <IngredientList ingredientList={props.recipe.ingredientList} />
-               </div>
-           </Card.Body>
-       </Card>
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Card>
+                <Card.Title>
+                    <Icon path={mdiGlassTulip} size={1} color="red" />{" "}
+                    {props.recipe.name}
+                    <Button variant="primary" onClick={handleShow} className="ml-2">
+                        Show Details
+                    </Button>
+                </Card.Title>
+                <Card.Body>
+                <div>
+                    <Icon path={mdiReceiptTextEditOutline} size={1} />
+                    {props.recipe.description}
+                </div>
+                <div>
+                    Ingredients: <IngredientList ingredientList={props.recipe.ingredientList} />
+                </div>
+            </Card.Body>
+            </Card>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{props.recipe.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <RecipeDetail recipe={props.recipe} />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
 
