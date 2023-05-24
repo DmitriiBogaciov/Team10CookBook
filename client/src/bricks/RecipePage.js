044679e03ca91901d3f2137e7d7bf2149ebb7871
Dmitrii Bogaciov <dmitrii.bogaciov@gmail.com>
2023-05-24 14:09:26 +0200

    added rating

--- client/src/bricks/RecipePage.js
+++ client/src/bricks/RecipePage.js
@@ -148,13 +148,37 @@ function RecipePage() {
                     <p>Your rating: {rating} stars</p>
                 ) : (
                     // Кнопки для оценки
-                    <div>
-                        <Button variant="outline-secondary" onClick={() => handleRateRecipe(1)}>1 star</Button>{' '}
-                        <Button variant="outline-secondary" onClick={() => handleRateRecipe(2)}>2 stars</Button>{' '}
-                        <Button variant="outline-secondary" onClick={() => handleRateRecipe(3)}>3 stars</Button>{' '}
-                        <Button variant="outline-secondary" onClick={() => handleRateRecipe(4)}>4 stars</Button>{' '}
-                        <Button variant="outline-secondary" onClick={() => handleRateRecipe(5)}>5 stars</Button>
-                    </div>
+                        <fieldset className="rating">
+                            <input type="radio" id="star5" name="rating" value="5" onClick={() => handleRateRecipe(5)} />
+                            <label className="full" htmlFor="star5" title="Awesome - 5 stars"></label>
+
+                            <input type="radio" id="star4half" name="rating" value="4 and a half" onClick={() => handleRateRecipe(4.5)} />
+                            <label className="half" htmlFor="star4half" title="Pretty good - 4.5 stars"></label>
+
+                            <input type="radio" id="star4" name="rating" value="4" onClick={() => handleRateRecipe(4)} />
+                            <label className="full" htmlFor="star4" title="Pretty good - 4 stars"></label>
+
+                            <input type="radio" id="star3half" name="rating" value="3 and a half" onClick={() => handleRateRecipe(3.5)} />
+                            <label className="half" htmlFor="star3half" title="Meh - 3.5 stars"></label>
+
+                            <input type="radio" id="star3" name="rating" value="3" onClick={() => handleRateRecipe(3)} />
+                            <label className="full" htmlFor="star3" title="Meh - 3 stars"></label>
+
+                            <input type="radio" id="star2half" name="rating" value="2 and a half" onClick={() => handleRateRecipe(2.5)} />
+                            <label className="half" htmlFor="star2half" title="Kinda bad - 2.5 stars"></label>
+
+                            <input type="radio" id="star2" name="rating" value="2" onClick={() => handleRateRecipe(2)} />
+                            <label className="full" htmlFor="star2" title="Kinda bad - 2 stars"></label>
+
+                            <input type="radio" id="star1half" name="rating" value="1 and a half" onClick={() => handleRateRecipe(1.5)} />
+                            <label className="half" htmlFor="star1half" title="Meh - 1.5 stars"></label>
+
+                            <input type="radio" id="star1" name="rating" value="1" onClick={() => handleRateRecipe(1)} />
+                            <label className="full" htmlFor="star1" title="Sucks big time - 1 star"></label>
+
+                            <input type="radio" id="starhalf" name="rating" value="half" onClick={() => handleRateRecipe(0.5)} />
+                            <label className="half" htmlFor="starhalf" title="Sucks big time - 0.5 stars"></label>
+                        </fieldset>
                 )}
             </div>
             <div className="row">
